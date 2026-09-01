59d69306d78b239346af09114268be4977344c62
LeonardoPiolla <leonardo.piolla@hotmail.com>
2026-09-01 18:33:10 -0300

    Create BootReceiver.kt

---
+++ app/src/main/java/com/automapoko/app/engine/receivers/BootReceiver.kt
@@ -0,0 +1,9 @@
+package com.automapoko.app.engine.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {}
+}
