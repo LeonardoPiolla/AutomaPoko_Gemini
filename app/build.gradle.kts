4f470ff40bce22555dcb6dfb35ef6d177fb1f0d8
LeonardoPiolla <leonardo.piolla@hotmail.com>
2026-09-23 13:47:10 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
 }
